acdb42a3d33dc179e6df7aa5c0acc553f66f8f07
Pchenrique <pchenrique1998@hotmail.com>
2021-01-13 03:06:57 +0000

    Deploying to gh-pages from  @ 26b05bb9a23dbb0cb83b42b4e3eb0bed90dcb6b0 🚀

---
+++ precache-manifest.c30a676032670b0937a81908cc0ea532.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33a352ab6731bdb3dcb6666752daed0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e961974c194ac4ac9536",
+    "url": "/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "bc8aea82d90572481822",
+    "url": "/static/css/4.a922a401.chunk.css"
+  },
+  {
+    "revision": "0b0169e5b9de77e38b84",
+    "url": "/static/css/main.af23b6d0.chunk.css"
+  },
+  {
+    "revision": "2b8fe839cf132ccbf9f0",
+    "url": "/static/js/2.153b30ba.chunk.js"
+  },
+  {
+    "revision": "0a6d64cd3b49be6a14afdf21fa7c6d43",
+    "url": "/static/js/2.153b30ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e961974c194ac4ac9536",
+    "url": "/static/js/3.481fe23f.chunk.js"
+  },
+  {
+    "revision": "bc8aea82d90572481822",
+    "url": "/static/js/4.50d02395.chunk.js"
+  },
+  {
+    "revision": "0b0169e5b9de77e38b84",
+    "url": "/static/js/main.07942d67.chunk.js"
+  },
+  {
+    "revision": "3c2221b1d8158404e7dc",
+    "url": "/static/js/runtime-main.a87ade3e.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "617e0f1b1b7b99b82926314c651367d2",
+    "url": "/static/media/agility.617e0f1b.png"
+  },
+  {
+    "revision": "f09b167b72e2f02be8801ca63767191b",
+    "url": "/static/media/barriga.f09b167b.jpg"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "1c0f61a3c25a67454de38dcde6456f3b",
+    "url": "/static/media/foodnow.1c0f61a3.jpg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "326e6b18843e05c733f42aa85974519c",
+    "url": "/static/media/ifce.326e6b18.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "a17913df0a4e19777e26d0ef87f8c507",
+    "url": "/static/media/uevora.a17913df.png"
+  },
+  {
+    "revision": "ea9fb1c35f4133b6fdbda33dd547fc60",
+    "url": "/static/media/uglyphoto.ea9fb1c3.jpg"
+  }
+]);
